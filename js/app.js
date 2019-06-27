17f0fb6829c77e8b12d45414ce52e3689ba9ce48
TiagoVeri <tiagoverisjc@gmail.com>
2019-06-27 12:52:33 -0300

    adicionado gerar lista de despesas ao digitar valores nos inputs

--- js/app.js
+++ js/app.js
@@ -53,6 +53,56 @@ class UI {
         }
 
     }
+
+    submitExpenseForm() {
+        const expenseValue = this.expenseInput.value;
+        const amountValue = this.amountInput.value;
+        if (expenseValue === "" || amountValue === "" || amountValue < 0) {
+            this.expenseFeedback.classList.add("showItem");
+            this.expenseFeedback.innerHTML = `<p>Valor não pode ser vazio ou negativo</p>`;
+            const self = this;
+            setTimeout(function () {
+                self.expenseFeedback.classList.remove("showItem");
+            }, 4000);
+
+        } else {
+            let amount = parseInt(amountValue);
+            this.expenseInput.value = '';
+            this.amountInput.value = '';
+
+            //objeto expense para ser usado na Lista de gastos
+            let expense = {
+                id: this.itemID,
+                title: expenseValue,
+                amount: amount,
+            }
+            //construindo lista de gastos
+            this.itemID++;
+            this.itemList.push(expense);
+            this.addExpense(expense);
+            //Show Balance em construção
+
+        }
+    }
+    addExpense(expense) {
+        const div = document.createElement('div');
+        div.classList.add('expense');
+        div.innerHTML = `
+        <div class="expense-item d-flex justify-content-between align-items-baseline">
+             <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
+             <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+             <div class="expense-icons list-item">
+             <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+                <i class="fas fa-edit"></i>
+             </a>
+             <a href="#" class="delete-icon" data-id="${expense.id}">
+                <i class="fas fa-trash"></i>
+             </a>
+             </div>
+        </div>
+        `;
+        this.expenseList.appendChild(div);
+    }
     totalExpense() {
         //valor temporário pois falta mais coisas no projeto.
         let total = 400;
@@ -76,6 +126,7 @@ function eventListenters() {
     //expense form submit
     expenseForm.addEventListener('submit', function (event) {
         event.preventDefault();
+        ui.submitExpenseForm();
     })
     //expense click 
     expenseList.addEventListener('click', function (event) {
